7ffcbe89cb66f147d7d65f13850cd083b61470b1
BrendonChirume <chirumebrendon7@gmail.com>
2023-08-27 19:06:21 +0200

    types

---
+++ lib/types.ts
@@ -0,0 +1,12 @@
+export interface Paper {
+    title: string;
+    course: string;
+    courseCode: string;
+    year: number;
+    collage: string;
+    faculty: string;
+    department: string;
+    paperType: string;
+    file?: string;
+    description?: string;
+}
