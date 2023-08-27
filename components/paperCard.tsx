84c7d1523d0e4f996128f01e773e7bb51daf5038
BrendonChirume <chirumebrendon7@gmail.com>
2023-08-27 19:13:19 +0200

    wp

---
+++ components/paperCard.tsx
@@ -0,0 +1,67 @@
+import Card from "@mui/joy/Card";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Typography from "@mui/joy/Typography";
+import Box from "@mui/joy/Box";
+import Link from "@mui/joy/Link";
+
+interface FileCardProps {
+    paper: { id: string, title: string, courseId: string }
+}
+
+const truncate = (token: string) => {
+    const tokenArray = token.split(' ');
+    const averageWordLen = Math.floor(tokenArray.reduce((acc, crr) => acc + crr.length, 0) / tokenArray.length);
+
+    if (averageWordLen > 4) {
+        return `${token.slice(0, 47)}...`;
+    }
+    return token
+}
+
+export default function PaperCard({paper}: FileCardProps) {
+    return (
+        <Card
+            variant="outlined"
+            component={Link}
+            underline={"none"}
+            href={`/library/course/${paper.title}`}
+            sx={{
+                '--Card-radius': (theme) => theme.vars.radius.sm,
+                boxShadow: 'none',
+                cursor: 'pointer'
+            }}
+        >
+            <CardOverflow
+                sx={{
+                    borderBottom: '1px solid',
+                    borderColor: 'neutral.outlinedBorder',
+                }}
+            >
+                <AspectRatio ratio="4/3" color="primary">
+                    <Typography
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            color: 'primary.plainColor',
+                        }}
+                    >
+                        .zip
+                    </Typography>
+                </AspectRatio>
+            </CardOverflow>
+            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <Box sx={{flex: 1, maxHeight: 72}}>
+                    <Typography level="title-sm" sx={{textTransform: 'capitalize'}}>
+                        {truncate(paper.title)}
+                    </Typography><br/>
+                    <Typography level="body-xs" mt={0.5}>
+                        Added 25 May 2011
+                    </Typography>
+                </Box>
+            </Box>
+        </Card>
+
+    )
+}
