1eab222eadeab25a24df9a3e813510365a37cc3d
BrendonChirume <chirumebrendon7@gmail.com>
2023-09-11 02:17:28 +0200

    signup

--- components/stracture.tsx
+++ components/stracture.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react';
-import Box, {BoxProps} from '@mui/joy/Box';
+import Box, { BoxProps } from '@mui/joy/Box';
 import Sheet from '@mui/joy/Sheet';
 
 function Root(props: BoxProps) {
     return (
         <Box
             {...props}
-            sx={
-                [
-                    {
-                        display: 'grid',
-                        gridTemplateColumns: '300px 1fr',
-                        gridTemplateRows: '64px 1fr',
-                        height: '100vh',
+            sx={[
+                {
+                    display: 'grid',
+                    gridTemplateColumns: {
+                        xs: '1fr',
+                        sm: 'minmax(64px, 200px) minmax(450px, 1fr)',
+                        md: 'minmax(160px, 300px) minmax(300px, 500px) minmax(500px, 1fr)',
                     },
-                    ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
-                ]
-            }
+                    gridTemplateRows: '64px 1fr',
+                    minHeight: '100vh',
+                },
+                ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
+            ]}
         />
     );
 }
@@ -26,32 +28,27 @@ function Header(props: BoxProps) {
         <Box
             component="header"
             className="Header"
-            {...
-                props
-            }
-            sx={
-                [
-                    {
-                        py: 1, px: 2,
-                        gap: 1,
-                        bgcolor: 'background.surface',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        gridColumn: '1 / -1',
-                        borderBottom: '1px solid',
-                        borderColor: 'divider',
-                        position: 'sticky',
-                        top: 0,
-                        zIndex: 1501,
-                    },
-                    ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
-                ]
-            }
+            {...props}
+            sx={[
+                {
+                    p: 2,
+                    gap: 2,
+                    bgcolor: 'background.surface',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    gridColumn: '1 / -1',
+                    borderBottom: '1px solid',
+                    borderColor: 'divider',
+                    position: 'sticky',
+                    top: 0,
+                    zIndex: 1100,
+                },
+                ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
+            ]}
         />
-    )
-        ;
+    );
 }
 
 function SideNav(props: BoxProps) {
@@ -59,56 +56,43 @@ function SideNav(props: BoxProps) {
         <Box
             component="nav"
             className="Navigation"
-            {...
-                props
-            }
-            sx={
-                [
-                    {
-                        p: 2,
-                        bgcolor: 'background.surface',
-                        borderRight: '1px solid',
-                        borderColor: 'divider',
-                        position: 'sticky',
-                        top: 60,
-                        height: 'calc(100vh - 60px)',
-                        display: {
-                            xs: 'none',
-                            md: 'initial',
-                        },
+            {...props}
+            sx={[
+                {
+                    p: 2,
+                    bgcolor: 'background.surface',
+                    borderRight: '1px solid',
+                    borderColor: 'divider',
+                    display: {
+                        xs: 'none',
+                        sm: 'initial',
                     },
-                    ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
-                ]
-            }
+                },
+                ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
+            ]}
         />
-    )
-        ;
+    );
 }
 
 function SidePane(props: BoxProps) {
     return (
         <Box
-            className="Filter"
-            {...
-                props
-            }
-            sx={
-                [
-                    {
-                        bgcolor: 'background.surface',
-                        borderRight: '1px solid',
-                        borderColor: 'divider',
-                        display: {
-                            xs: 'none',
-                            md: 'initial',
-                        },
+            className="Inbox"
+            {...props}
+            sx={[
+                {
+                    bgcolor: 'background.surface',
+                    borderRight: '1px solid',
+                    borderColor: 'divider',
+                    display: {
+                        xs: 'none',
+                        md: 'initial',
                     },
-                    ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
-                ]
-            }
+                },
+                ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
+            ]}
         />
-    )
-        ;
+    );
 }
 
 function Main(props: BoxProps) {
@@ -117,70 +101,49 @@ function Main(props: BoxProps) {
             component="main"
             className="Main"
             {...props}
-            sx={[{p: {xs: 2, md: 4}, height: 'calc(100vh - 64px)', overflowX: 'hidden', overflowY: 'auto'}, ...(Array.isArray(props.sx) ? props.sx : [props.sx])
-            ]
-            }
+            sx={[{ p: 2 }, ...(Array.isArray(props.sx) ? props.sx : [props.sx])]}
         />
-    )
-        ;
+    );
 }
 
-function SideDrawer(
-    {
-        onClose,
-        ...props
-    }: BoxProps & { onClose: React.MouseEventHandler<HTMLDivElement> }) {
+function SideDrawer({
+                        onClose,
+                        ...props
+                    }: BoxProps & { onClose: React.MouseEventHandler<HTMLDivElement> }) {
     return (
         <Box
             {...props}
-            sx={
-                [
-                    {position: 'fixed', zIndex: 1200, width: '100%', height: '100%'},
-                    ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
-                ]
-            }
+            sx={[
+                { position: 'fixed', zIndex: 1200, width: '100%', height: '100%' },
+                ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
+            ]}
         >
             <Box
                 role="button"
                 onClick={onClose}
-                sx={
-                    {
-                        position: 'absolute',
-                        inset:
-                            0,
-                        bgcolor:
-                            (theme) =>
-                                `rgba(${theme.vars.palette.neutral.darkChannel} / 0.8)`,
-                    }
-                }
+                sx={{
+                    position: 'absolute',
+                    inset: 0,
+                    bgcolor: (theme) =>
+                        `rgba(${theme.vars.palette.neutral.darkChannel} / 0.8)`,
+                }}
             />
-            < Sheet
-                sx={
-                    {
-                        minWidth: 256,
-                        width:
-                            'max-content',
-                        height:
-                            '100%',
-                        p:
-                            2,
-                        boxShadow:
-                            'lg',
-                        bgcolor:
-                            'background.surface',
-                    }
-                }
+            <Sheet
+                sx={{
+                    minWidth: 256,
+                    width: 'max-content',
+                    height: '100%',
+                    p: 2,
+                    boxShadow: 'lg',
+                    bgcolor: 'background.surface',
+                }}
             >
-                {
-                    props.children
-                }
+                {props.children}
             </Sheet>
         </Box>
-    )
-        ;
+    );
 }
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     Root,
     Header,
--- providers/studysynk.tsx
+++ providers/studysynk.tsx
@@ -8,7 +8,7 @@ import {Provider} from 'react-redux'
 import {usePathname, useRouter} from "next/navigation";
 import Navbar from "@/components/navbar";
 import EnhancedBreadcrumbs from "@/components/breadcrumbs";
-import {useFilter} from "@/containers/filterContext";
+import {useFilter} from "@/context/filterContext";
 import Typography from "@mui/joy/Typography";
 import Divider from "@mui/joy/Divider";
 import Box from "@mui/joy/Box";
@@ -20,6 +20,7 @@ import Sheet from "@mui/joy/Sheet";
 import {AvatarGroup} from "@mui/joy";
 import Avatar from "@mui/joy/Avatar";
 import AspectRatio from "@mui/joy/AspectRatio";
+import {AuthProvider} from "@/providers/AuthProvider";
 
 interface StudySynkProps {
     children: React.ReactNode;
@@ -32,133 +33,136 @@ export default function Studysynk({children}: StudySynkProps) {
     const {isOpen, showFilter} = useFilter();
 
     return (
-        <Provider store={store}>
-            {drawerOpen && (
-                <Layout.SideDrawer onClose={() => setDrawerOpen(false)}>
-                    <Sidebar navigate={navigate.push} currentRoute={currentRoute}/>
-                </Layout.SideDrawer>
-            )}
-            <Layout.Root
-                sx={{
-                    gridTemplateColumns: isOpen ? {xs: '100%', md: "300px 1fr minmax(280px, 300px)"} : {
-                        xs: '100%',
-                        md: "300px 1fr"
-                    },
-                    // transform: 'translateX(calc(100% * (var(1, 0) - 1)))',
-                    transition: '0.3s ease',
-                    ...(drawerOpen && {
-                        height: '100vh',
-                        overflow: 'hidden',
-                    }),
-                }}
-            >
-                <Layout.Header>
-                    <Navbar setDrawerOpen={setDrawerOpen}/>
-                </Layout.Header>
-                <Layout.SideNav>
-                    <Sidebar navigate={navigate.push} currentRoute={currentRoute}/>
-                </Layout.SideNav>
-                <Layout.Main>
-                    <EnhancedBreadcrumbs pathname={currentRoute}/>
-                    {children}
-                </Layout.Main>
-                <Sheet
-                    sx={{
-                        display: {xs: 'none', sm: 'initial'},
-                        borderLeft: '1px solid',
-                        borderColor: 'neutral.outlinedBorder',
-                    }}
-                >
-                    <Box sx={{p: 2, display: 'flex', alignItems: 'center'}}>
-                        <Typography level="h5" sx={{flex: 1}}>Filters</Typography>
-                        <IconButton variant="outlined" color="neutral" size="sm" onClick={() => showFilter(!isOpen)}>
-                            <XMarkIcon className="w-4 h-4 ss-icon"/>
-                        </IconButton>
-                    </Box>
-                    <Divider/>
-                    <AspectRatio ratio="21/9">
-                        {/*<img*/}
-                        {/*    alt=""*/}
-                        {/*    src="https://images.unsplash.com/photo-1534067783941-51c9c23ecefd?auto=format&fit=crop&w=774"*/}
-                        {/*/>*/}
-                    </AspectRatio>
-                    <Box sx={{p: 2, display: 'flex', gap: 1, alignItems: 'center'}}>
-                        <Typography level="body-sm" mr={1}>
-                            Shared with
-                        </Typography>
-                        <AvatarGroup size="sm" sx={{'--Avatar-size': '24px'}}>
-                            <Avatar
-                                src="https://i.pravatar.cc/24?img=6"
-                                srcSet="https://i.pravatar.cc/48?img=6 2x"
-                            />
-                            <Avatar
-                                src="https://i.pravatar.cc/24?img=7"
-                                srcSet="https://i.pravatar.cc/48?img=7 2x"
-                            />
-                            <Avatar
-                                src="https://i.pravatar.cc/24?img=8"
-                                srcSet="https://i.pravatar.cc/48?img=8 2x"
-                            />
-                            <Avatar
-                                src="https://i.pravatar.cc/24?img=9"
-                                srcSet="https://i.pravatar.cc/48?img=9 2x"
-                            />
-                        </AvatarGroup>
-                    </Box>
-                    <Divider/>
-                    <Box
+        <AuthProvider>
+            {['/signin', '/signup'].includes(currentRoute) ? children : (
+                <Provider store={store}>
+                    {drawerOpen && (
+                        <Layout.SideDrawer onClose={() => setDrawerOpen(false)}>
+                            <Sidebar navigate={navigate.push} currentRoute={currentRoute}/>
+                        </Layout.SideDrawer>
+                    )}
+                    <Layout.Root
                         sx={{
-                            gap: 2,
-                            p: 2,
-                            display: 'grid',
-                            gridTemplateColumns: 'auto 1fr',
-                            '& > *:nth-child(odd)': {color: 'text.secondary'},
+                            gridTemplateColumns: {
+                                xs: '1fr',
+                                sm: `minmax(160px, 300px) minmax(600px, 1fr) ${isOpen ? "minmax(280px, 300px)" : ""}`
+                            },
+                            ...(drawerOpen && {
+                                height: '100vh',
+                                overflow: 'hidden',
+                            }),
                         }}
                     >
-                        <Typography level="body-sm">Type</Typography>
-                        <Typography level="body-sm" textColor="text.primary">
-                            Image
-                        </Typography>
+                        <Layout.Header>
+                            <Navbar setDrawerOpen={setDrawerOpen}/>
+                        </Layout.Header>
+                        <Layout.SideNav>
+                            <Sidebar navigate={navigate.push} currentRoute={currentRoute}/>
+                        </Layout.SideNav>
+                        <Layout.Main>
+                            <EnhancedBreadcrumbs pathname={currentRoute}/>
+                            {children}
+                        </Layout.Main>
+                        <Sheet
+                            sx={{
+                                display: {xs: 'none', sm: isOpen ? 'initial' : 'none'},
+                                borderLeft: '1px solid',
+                                borderColor: 'neutral.outlinedBorder',
+                            }}
+                        >
+                            <Box sx={{p: 2, display: 'flex', alignItems: 'center'}}>
+                                <Typography level="h5" sx={{flex: 1}}>Filters</Typography>
+                                <IconButton variant="outlined" color="neutral" size="sm"
+                                            onClick={() => showFilter(!isOpen)}>
+                                    <XMarkIcon className="w-4 h-4 ss-icon"/>
+                                </IconButton>
+                            </Box>
+                            <Divider/>
+                            <AspectRatio ratio="21/9">
+                                {/*<img*/}
+                                {/*    alt=""*/}
+                                {/*    src="https://images.unsplash.com/photo-1534067783941-51c9c23ecefd?auto=format&fit=crop&w=774"*/}
+                                {/*/>*/}
+                            </AspectRatio>
+                            <Box sx={{p: 2, display: 'flex', gap: 1, alignItems: 'center'}}>
+                                <Typography level="body-sm" mr={1}>
+                                    Shared with
+                                </Typography>
+                                <AvatarGroup size="sm" sx={{'--Avatar-size': '24px'}}>
+                                    <Avatar
+                                        src="https://i.pravatar.cc/24?img=6"
+                                        srcSet="https://i.pravatar.cc/48?img=6 2x"
+                                    />
+                                    <Avatar
+                                        src="https://i.pravatar.cc/24?img=7"
+                                        srcSet="https://i.pravatar.cc/48?img=7 2x"
+                                    />
+                                    <Avatar
+                                        src="https://i.pravatar.cc/24?img=8"
+                                        srcSet="https://i.pravatar.cc/48?img=8 2x"
+                                    />
+                                    <Avatar
+                                        src="https://i.pravatar.cc/24?img=9"
+                                        srcSet="https://i.pravatar.cc/48?img=9 2x"
+                                    />
+                                </AvatarGroup>
+                            </Box>
+                            <Divider/>
+                            <Box
+                                sx={{
+                                    gap: 2,
+                                    p: 2,
+                                    display: 'grid',
+                                    gridTemplateColumns: 'auto 1fr',
+                                    '& > *:nth-child(odd)': {color: 'text.secondary'},
+                                }}
+                            >
+                                <Typography level="body-sm">Type</Typography>
+                                <Typography level="body-sm" textColor="text.primary">
+                                    Image
+                                </Typography>
 
-                        <Typography level="body-sm">Size</Typography>
-                        <Typography level="body-sm" textColor="text.primary">
-                            3,6 MB (3,258,385 bytes)
-                        </Typography>
+                                <Typography level="body-sm">Size</Typography>
+                                <Typography level="body-sm" textColor="text.primary">
+                                    3,6 MB (3,258,385 bytes)
+                                </Typography>
 
-                        <Typography level="body-sm">Storage used</Typography>
-                        <Typography level="body-sm" textColor="text.primary">
-                            3,6 MB (3,258,385 bytes)
-                        </Typography>
+                                <Typography level="body-sm">Storage used</Typography>
+                                <Typography level="body-sm" textColor="text.primary">
+                                    3,6 MB (3,258,385 bytes)
+                                </Typography>
 
-                        <Typography level="body-sm">Location</Typography>
-                        <Typography level="body-sm" textColor="text.primary">
-                            Travel pictures
-                        </Typography>
+                                <Typography level="body-sm">Location</Typography>
+                                <Typography level="body-sm" textColor="text.primary">
+                                    Travel pictures
+                                </Typography>
 
-                        <Typography level="body-sm">Owner</Typography>
-                        <Typography level="body-sm" textColor="text.primary">
-                            Michael Scott
-                        </Typography>
+                                <Typography level="body-sm">Owner</Typography>
+                                <Typography level="body-sm" textColor="text.primary">
+                                    Michael Scott
+                                </Typography>
 
-                        <Typography level="body-sm">Modified</Typography>
-                        <Typography level="body-sm" textColor="text.primary">
-                            26 October 2016
-                        </Typography>
+                                <Typography level="body-sm">Modified</Typography>
+                                <Typography level="body-sm" textColor="text.primary">
+                                    26 October 2016
+                                </Typography>
 
-                        <Typography level="body-sm">Created</Typography>
-                        <Typography level="body-sm" textColor="text.primary">
-                            5 August 2016
-                        </Typography>
-                    </Box>
-                    <Divider/>
-                    <Box sx={{py: 2, px: 1}}>
-                        <Button variant="plain" size="sm" endDecorator={<PencilSquareIcon/>}>
-                            Add a description
-                        </Button>
-                    </Box>
-                </Sheet>
+                                <Typography level="body-sm">Created</Typography>
+                                <Typography level="body-sm" textColor="text.primary">
+                                    5 August 2016
+                                </Typography>
+                            </Box>
+                            <Divider/>
+                            <Box sx={{py: 2, px: 1}}>
+                                <Button variant="plain" size="sm" endDecorator={<PencilSquareIcon/>}>
+                                    Add a description
+                                </Button>
+                            </Box>
+                        </Sheet>
 
-            </Layout.Root>
-        </Provider>
+                    </Layout.Root>
+                </Provider>
+            )}
+        </AuthProvider>
     );
 }
