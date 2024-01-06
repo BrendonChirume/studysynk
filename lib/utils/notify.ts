429ce72e9330b97e7f04105555e8cdae45381152
BrendonChirume <chirumebrendon7@gmail.com>
2024-01-06 15:24:10 +0200

    bug fix

--- lib/utils/notify.ts
+++ lib/utils/notify.ts
@@ -1,14 +1,15 @@
 import {toast} from "react-toastify";
 
-const notify = (message: string, type: "success" | "error" | "warning") => toast[type](message, {
-    position: "bottom-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "dark",
-});
+export default function notify(message: string, type: "success" | "error" | "warning") {
+    return toast[type](`${message}`, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    });
+}
 
-export default notify;
