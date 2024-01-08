6ff3fba8b7950a5202241d47ba0072d92388144b
BrendonChirume <chirumebrendon7@gmail.com>
2024-01-08 19:23:42 +0200

    add faculty/university

--- app/api/faculties/route.ts
+++ app/api/faculties/route.ts
@@ -4,29 +4,36 @@ import {NextResponse} from "next/server";
 import {lower} from "@/lib/utils/helper";
 
 export async function POST(request: Request) {
-    const {uniId, ...rest} = await request.json();
+    const res = await request.json();
+    await lower(res, 'universityId');
+
     await connectMongoDB();
 
-    const isExist = await Faculty.findOne({name: rest.name}).select("_id");
+    // check if faculty exists
+    const isExist = await Faculty.findOne({name: res.name}).select("_id");
     if (isExist) {
         return NextResponse.json({message: "Faculty already exists!"});
     }
-    await lower(rest);
-    const faculty = await Faculty.create(rest);
-    const doc = await University.findById(uniId);
 
-    doc.faculties.push(faculty.id);
-    await doc.save();
+    // create faculty to database
+    const faculty = await Faculty.create(res);
+
+    // add faculty to university document
+    const university = await University.findById(res.university.id);
+
+    university.faculties.push(faculty.id);
+    await university.save();
 
     return NextResponse.json({message: "Faculty created successfully!"});
 }
 
 export async function GET(request: Request) {
     const {searchParams} = new URL(request.url)
-    const university = searchParams.get('university');
+    const universityId = searchParams.get('universityId');
+    console.log(universityId);
     await connectMongoDB();
-    if (university) {
-        const data = await Faculty.find({university});
+    if (universityId) {
+        const data = await Faculty.find({universityId});
         return NextResponse.json(data);
     }
     const data = await Faculty.find({});
